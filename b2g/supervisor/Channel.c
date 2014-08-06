b58f2835bf5aca0f22456c1776d32b127704d9ff
Julian Hector <jhector@mozilla.com>
2014-08-05 19:48:30 -0700

    added function to accept a client

--- b2g/supervisor/Channel.c
+++ b2g/supervisor/Channel.c
@@ -26,6 +26,8 @@ static int32_t sSockChannel = -1;
 enum ioresult EpollCbSockListen(int32_t aFd, uint32_t aEvent, void* aData);
 enum ioresult EpollCbSockChannel(int32_t aFd, uint32_t aEvent, void* aData);
 
+void ChannelOpen(struct ChannelDataCb* aCbData);
+
 // TODO: docu
 enum ioresult 
 EpollCbSockListen(int32_t aFd, uint32_t aEvent, void* aData)
@@ -39,10 +41,63 @@ EpollCbSockListen(int32_t aFd, uint32_t aEvent, void* aData)
   if (aEvent & EPOLLERR) {
     // TODO: close channel and die
   } else if (aEvent & EPOLLIN) {
-    // TODO: accept the connection
+    ChannelOpen(aCbData);
   }
 
   return IO_OK;
+}
+
+// TODO: docu
+enum ioresult
+EpollCbSockChannel(int32_t aFd, uint32_t aEvent, void* aData)
+{
+  return IO_OK;
+}
+
+// TODO: docu
+void ChannelOpen(struct ChannelDataCb* aCbData)
+{
+  char* err = NULL;
+
+  if (sSockChannel > 0) {
+    return;
+  }
+
+  sSockChannel = HANDLE_EINTR(accept(sSockListen, NULL, 0));
+  if (sSockChannel < 0) {
+    err = "failed to accept()";
+    goto error;
+  }
+
+  if (SetFdFlag(sSockChannel, O_NONBLOCK) < 0) {
+    err = "failed to set O_NONBLOCK flag";
+    goto error_close;
+  }
+
+  if (add_fd_to_epoll_loop(sSockChannel, EPOLLIN,
+        EpollCbSockChannel, aCbData) < 0) {
+    err = "failed to register epoll event for channel";
+    goto error_close;
+  }
+
+  if (add_fd_to_epoll_loop(sSockListen, EPOLLERR,
+        EpollCbSockListen, aCbData) < 0) {
+    err = "failed to register epoll event for listen";
+    goto error_close;
+  }
+
+  if (aCbData && aCbData->OnChannelOpened) {
+    aCbData->OnChannelOpened();
+  }
+
+  return;
+
+error_close:
+  HANDLE_EINTR(close(sSockChannel));
+  sSockChannel = -1;
+error:
+  // TODO: log err
+  return;
 }
 
 // TODO: docu

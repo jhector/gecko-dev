4af078ea25be10bfa7e9dcf818ebcf08b4831d75
Julian Hector <jhector@mozilla.com>
2014-08-11 14:39:22 -0700

    added support for wifi commands

--- b2g/supervisor/Types.c
+++ b2g/supervisor/Types.c
@@ -3,6 +3,9 @@
 /* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+#include <dlfcn.h>
+
+/* from b2g/supervisor/include */
 #include <ipc/Types.h>
 #include <ipc/Message.h>
 
@@ -16,6 +19,59 @@ HandleCmdWifi(struct SvMessage* aMsg)
 #ifdef DEBUG
   printf("Got wifi command: %s\n", aMsg->data);
 #endif
+  static void* wifilib = NULL;
+
+  uint32_t length = 0;
+  char* cmd = NULL;
+
+  enum SvTypeError err = SV_ERROR_OK;
+
+  void* iter = (void*)aMsg->data;
+
+  if (ReadString(iter, &cmd, &length) < 0) {
+    err = SV_ERROR_FAILED;
+    goto respond;
+  }
+
+  if (length == 0) {
+    err = SV_ERROR_MISSING;
+    goto respond;
+  } else if (length > aMsg->header.size) {
+    err = SV_ERROR_INVALID;
+    goto respond;
+  }
+
+  /* |cmd| contains a valid string */
+  if (strcmp(cmd, "wifi_load_driver")) {
+    err = SV_ERROR_DENIED;
+    goto respond;
+  }
+
+  /* |cmd| is in whitelist get function and call it */
+  if (!wifilib) {
+    wifilib = dlopen("/system/lib/libhardware_legacy.so", RTLD_LAZY);
+    if (!wifilib) {
+      err = SV_ERROR_FAILED;
+      goto respond;
+    }
+  }
+
+  int (*func)();
+  func = (int (*)())dlsym(wifilib, cmd);
+
+  if (dlerror() != NULL) {
+    err = SV_ERROR_FAILED;
+    goto respond;
+  }
+
+  if ((*func)() < 0) {
+    err = SV_ERROR_FAILED;
+    goto respond;
+  }
+
+respond:
+  // TODO: send respond message
+  return;
 }
 
 void

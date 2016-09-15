3c68a059a08a57630f71498f7d2dc29726a5d783
Julian Hector <julian.r.hector@gmail.com>
2016-09-15 13:10:17 +0000

    Bug 1104619 - List all functions that needs remoting

--- dom/media/systemservices/AudioChild.h
+++ dom/media/systemservices/AudioChild.h
@@ -23,9 +23,53 @@ struct cubeb {
 namespace mozilla {
 namespace audio {
 
+
 int Init(cubeb** aContext, char const* aContextName);
 const char* GetBackendId(cubeb* aContext);
+/*
+int GetMaxChannelCount(cubeb* aContext, uint32_t* aMaxChannels);
+int GetMinLatency(cubeb* aContext,
+                  cubeb_stream_params aParams,
+                  uint32_t* aLatencyFrame);
+int GetPreferredSampleRate(cubeb* aContext, uint32_t* aRate);
+*/
 void Destroy(cubeb* aContext);
+/*
+int StreamInit(cubeb* aContext,
+               cubeb_stream** aStream,
+               char const* aStreamName,
+               cubeb_devid input_device,
+               cubeb_stream_params* aInputStreamParams,
+               cubeb_devid output_device,
+               cubeb_stream_params* aOutputStreamParams,
+               unsigned int aLatencyFrames,
+               cubeb_data_callback aDataCallback,
+               cubeb_state_callback aStateCallback,
+               void* aUserPtr);
+void StreamDestroy(cubeb_stream* aStream);
+int StreamStart(cubeb_stream* aStream);
+int StreamStop(cubeb_stream* aStream);
+int StreamGetPosition(cubeb_stream* aStream, uint64_t* aPosition);
+int StreamGetLatency(cubeb_stream* aStream, uint32_t* aLatency);
+int StreamSetVolume(cubeb_stream* aStream, float aVolume);
+int StreamSetPanning(cubeb_stream* aStream, int aPanning);
+int StreamGetCurrentDevice(cubeb_stream* aStream,
+                           cubeb_device** const aDevice);
+int StreamDeviceDestroy(cubeb_stream* aStream, cubeb_device* aDevices);
+int StreamRegisterDeviceChangedCallback(
+  cubeb_stream* aStream,
+  cubeb_device_changed_callback aDeviceChangedCallback);
+int EnumerateDevices(cubeb* aContext,
+                     cubeb_device_type aDevtype,
+                     cubeb_device_collection** aCollection);
+int DeviceCollectionDestroy(cubeb_device_collection* aCollection);
+int DeviceInfoDestroy(cubeb_device_info* aInfo);
+int RegisterDeviceCollectionChanged(
+  cubeb* aContext,
+  cubeb_device_type aDevtype,
+  cubeb_device_collection_changed_callback aCallback,
+  void* aUserPtr);
+*/
 
 class AudioChild : public PAudioChild
 {
